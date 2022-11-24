c74660e748c1feaa535413df25e2fc805bf3ae49
Judy <gkfajsl1225@naver.com>
2022-11-24 15:28:53 +0900

    judyTimes 수정 + 리액트 프로젝트 생성

--- JavaScript/Judy-Times/main.js
+++ JavaScript/Judy-Times/main.js
@@ -10,6 +10,43 @@ const getLatestNews = async () => {
     let data = await response.json();
     news = data.articles;
     console.log(news);
+
+    render();
+};
+
+const render = () => {
+    let newsHTML = "";
+    newsHTML = news
+        .map((item) => {
+            return `<div class="row news">
+        <div class="col-lg-4">
+            <img
+                class="news-img-size"
+                src="${
+                    news.media ||
+                    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRqEWgS0uxxEYJ0PsOb2OgwyWvC0Gjp8NUdPw&usqp=CAU"
+                }"/>
+        </div>
+        <div class="col-lg-8">
+            <h2>${item.title}</h2>
+            <p>
+            news.summary == null || news.summary == ""
+            ? "내용없음"
+            : news.summary.length > 200
+            ? news.summary.substring(0, 200) + "..."
+            : news.summary
+            </p>
+            <div>${news.rights || "no source"}  ${moment(
+                news.published_date
+            ).fromNow()}</div>
+        </div>
+    </div>`;
+        })
+        .join("");
+
+    console.log(newsHTML);
+
+    document.getElementById("news-board").innerHTML = newsHTML;
 };
 
 getLatestNews();
