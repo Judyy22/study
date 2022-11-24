79209fa677e90855e8642fa247797a5c8e455981
Judy <gkfajsl1225@naver.com>
2022-11-24 15:42:53 +0900

    component 만들기

---
+++ React/first-react/src/component/Box.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Box = () => {
+    return (
+        <div className="box">
+            Box1
+            <p>리사</p>
+        </div>
+    );
+};
+
+export default Box;
