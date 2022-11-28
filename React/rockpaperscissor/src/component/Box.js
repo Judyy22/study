38c421b99782b0145228e95acc7256c51314aaff
Judy <gkfajsl1225@naver.com>
2022-11-28 21:02:52 +0900

    YHtour 위치 이동 + 가위바위보 끝냄

--- React/rockpaperscissor/src/component/Box.js
+++ React/rockpaperscissor/src/component/Box.js
@@ -1,11 +1,22 @@
 import React from "react";
 
 const Box = (props) => {
+    let result;
+    if (
+        props.title == "computer" &&
+        props.result !== "tie" &&
+        props.result !== ""
+    ) {
+        result = props.result == "win" ? "lose" : "win";
+    } else {
+        result = props.result;
+    }
     return (
-        <div className="box">
+        <div className={`box ${result}`}>
             <h1>{props.title}</h1>
+            <h2 data-testid="item-name">{props.item && props.item.name}</h2>
             <img className="item-img" src={props.item && props.item.img} />
-            <h2>WIN</h2>
+            <h2>{result}</h2>
         </div>
     );
 };
