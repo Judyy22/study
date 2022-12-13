ed722d46ef8c315cd9273a86ad7ce119e1f64e16
Judyy22 <115631038+Judyy22@users.noreply.github.com>
2022-12-13 19:46:24 +0900

    디테일 페이지 거의 함 리뷰/관련영상 해야함

--- React/netflix/src/component/Navigation.js
+++ React/netflix/src/component/Navigation.js
@@ -6,11 +6,12 @@ const Navigation = () => {
     return (
         <Navbar className="nav-background" variant="dark" expand="lg">
             <Container fluid>
-                <Navbar.Brand href="#">
+                <Navbar.Brand>
+                    <Link to="/">
                     <img
                         width={100}
                         src="https://www.edigitalagency.com.au/wp-content/uploads/Netflix-logo-red-black-png.png"
-                    />
+                    /></Link>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
                 <Navbar.Collapse id="navbarScroll">
--- React/netflix/src/pages/MovieDetail.js
+++ React/netflix/src/pages/MovieDetail.js
@@ -1,7 +1,10 @@
 import React, { useEffect, useState } from "react";
-import { useDispatch, useSelector } from "react-redux";
 import { useParams } from "react-router-dom";
-import { movieAction } from "../redux/Actions/movieAction";
+import { Badge } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { faUsers } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { Review } from "../component/Review";
 
 const MovieDetail = () => {
     const API_KEY = process.env.REACT_APP_API_KEY;
@@ -20,8 +23,71 @@ const MovieDetail = () => {
     }, []);
 
     return (
-        <div>
-            <div>${movie.original_title}</div>
+        <div className="container">
+            <div className="row movie-detail">
+                <div className="col-lg-6">
+                    <img
+                        className="movie-detail-img"
+                        src={`https://image.tmdb.org/t/p/original/${movie?.poster_path}`}
+                    />
+                </div>
+                <div className="col-lg-6 movie-detail-right">
+                    <div className="movie-detail-genre">
+                        {movie?.genres.map((genre) => (
+                            <Badge
+                                bg="danger"
+                                className="movie-detail-genre-own"
+                            >
+                                {genre.name}
+                            </Badge>
+                        ))}
+                    </div>
+                    <div className="movie-detail-first">
+                        <div className="movie-detail-title">
+                            {movie?.original_title}
+                        </div>
+                        <div className="movie-detail-tagline">
+                            {movie?.tagline}
+                        </div>
+                        <div>
+                            <span className="movie-detail-vote">
+                                ⭐{movie?.vote_average}
+                            </span>
+                            <span className="movie-detail-popularity">
+                                <FontAwesomeIcon
+                                    icon={faUsers}
+                                    className="fontawesome"
+                                />
+                                {movie?.popularity}
+                            </span>
+                            <span className="movie-detail-adulte">
+                                {movie?.adulte ? "청불" : "under 18"}
+                            </span>
+                        </div>
+                    </div>
+                    <div className="movie-detail-overview">
+                        {movie?.overview}
+                    </div>
+                    <div className="movie-detail-third">
+                        <div className="movie-detail-release">
+                            <Badge bg="danger" className="fontawesome">
+                                Release Day
+                            </Badge>
+                            {movie?.release_date}
+                        </div>
+                        <div className="movie-detail-release">
+                            <Badge bg="danger" className="fontawesome">
+                                Time
+                            </Badge>
+                            {movie?.runtime}
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-4"></div>
+                <div className="col-8">RELATED MOVIES</div>
+            </div>
         </div>
     );
 };
