b07c6d0b9c3d1e480c71de6122362853c56edfac
Seunghui Lee <115631038+Judyy22@users.noreply.github.com>
2023-01-13 15:30:25 +0900

    props children

---
+++ React/react-complete-guide/src/components/ExpenseItem.js
@@ -0,0 +1,18 @@
+import "./ExpenseItem.css";
+import React from "react";
+import ExpenseDate from "./ExpenseDate";
+import Card from "./Card";
+
+const ExpenseItem = (props) => {
+    return (
+        <Card className="expense-item">
+            <ExpenseDate date={props.date} />
+            <div className="expense-item__description">
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
+            </div>
+        </Card>
+    );
+};
+
+export default ExpenseItem;
