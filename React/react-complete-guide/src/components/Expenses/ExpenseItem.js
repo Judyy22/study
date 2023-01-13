6ddb9684e1c7dd548b8e59e6f13f2912d197fa81
Seunghui Lee <115631038+Judyy22@users.noreply.github.com>
2023-01-13 16:18:54 +0900

    react component test

--- React/react-complete-guide/src/components/Expenses/ExpenseItem.js
+++ React/react-complete-guide/src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import "./ExpenseItem.css";
 import React from "react";
 import ExpenseDate from "./ExpenseDate";
-import Card from "./Card";
+import Card from "../UI/Card";
 
 const ExpenseItem = (props) => {
     return (
--- React/react-complete-guide/src/components/UI/Card.js
+++ React/react-complete-guide/src/components/UI/Card.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./Card.css";
 
 const Card = (props) => {
-    const classes = "card" + props.className;
+    const classes = "card " + props.className;
     return <div className={classes}>{props.children}</div>;
 };
 
