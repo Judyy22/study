ade0e86a3579fc6c56b78d817744024215a6e404
Seunghui Lee <115631038+Judyy22@users.noreply.github.com>
2023-01-17 14:31:37 +0900

    State와 함께 일하기

--- React/react-complete-guide/src/components/Expenses/Expenses.js
+++ React/react-complete-guide/src/components/Expenses/Expenses.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
+import Card from "../UI/Card";
 
 const Expenses = (props) => {
     return (
-        <div className="expenses">
+        <Card className="expenses">
             <ExpenseItem
                 title={props.items[0].title}
                 amount={props.items[0].amount}
@@ -25,7 +26,7 @@ const Expenses = (props) => {
                 amount={props.items[3].amount}
                 date={props.items[3].date}
             />
-        </div>
+        </Card>
     );
 };
 
